e2f64246d87fa891498cd09da6dbf6e46c20e498
David <david.nenita@isa.utm.md>
2026-04-07 22:58:08 +0300

    Revert "Add more console.log on hello.js"
    This reverts commit 9cc4e760de1c0116009841710b9c6b1e8abb6d2e.

--- hello.js
+++ hello.js
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
 console.log("Hello, Git");
 console.log("Hello, GitHub");
 console.log("Trying revert!");
+=======
+console.log("Hello, Git");
+>>>>>>> parent of 9cc4e76 (Add more console.log on hello.js)
